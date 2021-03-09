a50069eda45d3474e0e576ac2093dcf0c0968cdf
ulys <maklumi@yahoo.com>
2021-03-09 19:22:59 +0800

    Ch02 All maps now have a collision layer. Make player detects this.

--- core/src/main/java/com/ulys/Entiti.kt
+++ core/src/main/java/com/ulys/Entiti.kt
@@ -1,15 +1,12 @@
 package com.ulys
 
 import com.badlogic.gdx.graphics.g2d.Animation
-import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.graphics.g2d.TextureRegion
+import com.badlogic.gdx.math.Rectangle
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.utils.Array
 
 open class Entiti(private val util: Util) {
-
-    private val lebarFrem = 16
-    private val tinggiFrem = 16
 
     private val spritePath = "sprites/Characters/Warrior.png"
     var texRegion: TextureRegion? = null
@@ -29,33 +26,41 @@ open class Entiti(private val util: Util) {
     private val walkUpAnim = Animation(0.25f, walkUpFrames, Animation.PlayMode.LOOP)
     private val walkDownAnim = Animation(0.25f, walkDownFrames, Animation.PlayMode.LOOP)
 
-    var pos = Vector2(1f, 1f)
-    val laju = Vector2(5f, 5f)
+    val pos = Vector2(1f, 1f)
+    private val laju = Vector2(5f, 5f)
 
     enum class Arah { KIRI, KANAN, ATAS, BAWAH }
 
     var arah = Arah.BAWAH
-
+    val nextRect = Rectangle()
+    private val nextPos = Vector2(pos)
 
     fun kemaskini(delta: Float) {
         masafrem = (masafrem + delta) % 5
         setCurrentFrame()
+        setNextBoundSize()
     }
 
     fun kiraPosisi(delta: Float) {
+        val tempPos = Vector2(pos)
         laju.scl(delta)
         when (arah) {
-            Arah.KIRI -> pos.x -= laju.x
-            Arah.KANAN -> pos.x += laju.x
-            Arah.ATAS -> pos.y += laju.y
-            Arah.BAWAH -> pos.y -= laju.y
+            Arah.KIRI -> tempPos.x -= laju.x
+            Arah.KANAN -> tempPos.x += laju.x
+            Arah.ATAS -> tempPos.y += laju.y
+            Arah.BAWAH -> tempPos.y -= laju.y
         }
+        nextPos.set(tempPos)
         laju.scl(1 / delta)
+    }
+
+    fun setCalculatedPosAsCurrent() {
+        pos.set(nextPos)
     }
 
     private fun muatSemuaAnimasi() {
         val tekstur = util.getAsetTekstur(spritePath)
-        val texRegions = TextureRegion.split(tekstur, lebarFrem, tinggiFrem)
+        val texRegions = TextureRegion.split(tekstur, LEBAR_FREM, TINGGI_FREM)
         for (row in 0..3) {
             for (col in 0..3) {
                 val textureRegion = texRegions[row][col]
@@ -76,5 +81,16 @@ open class Entiti(private val util: Util) {
             Arah.ATAS -> walkUpAnim.getKeyFrame(masafrem)
             Arah.BAWAH -> walkDownAnim.getKeyFrame(masafrem)
         }
+    }
+
+    private fun setNextBoundSize() {
+        val lebar = LEBAR_FREM * 1f
+        val tinggi = TINGGI_FREM * 0.5f
+        nextRect.set(nextPos.x, nextPos.y, lebar, tinggi)
+    }
+
+    companion object {
+        private const val LEBAR_FREM = 16
+        private const val TINGGI_FREM = 16
     }
 }
--- core/src/main/java/com/ulys/skrin/SkrinUtama.kt
+++ core/src/main/java/com/ulys/skrin/SkrinUtama.kt
@@ -4,25 +4,26 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Screen
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.OrthographicCamera
-import com.badlogic.gdx.graphics.g2d.Sprite
+import com.badlogic.gdx.maps.MapLayer
+import com.badlogic.gdx.maps.objects.RectangleMapObject
 import com.badlogic.gdx.maps.tiled.TiledMap
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer
+import com.badlogic.gdx.math.Rectangle
 import com.ulys.Entiti
 import com.ulys.KawalPemain
 import com.ulys.Util
 
 class SkrinUtama : Screen {
 
-    var lebarWindow = 0f
-    var tinggiWindow = 0f
-    var lebarViewcam = 10f
-    var tinggiViewcam = 10f
-    val kpp = 1 / 16f // 1 kaki = 16 piksel
-    var peta: TiledMap? = null
-    lateinit var kamera: OrthographicCamera
-    lateinit var renderer: OrthogonalTiledMapRenderer
+    private var lebarWindow = 0f
+    private var tinggiWindow = 0f
+    private var lebarViewcam = 10f
+    private var tinggiViewcam = 10f
+    private var peta: TiledMap? = null
+    private lateinit var kamera: OrthographicCamera
+    private lateinit var renderer: OrthogonalTiledMapRenderer
     private val util = Util()
-    private val path = "sprites/tmx/Town.tmx"
+    private val path = "maps/town.tmx"
     private lateinit var player: Entiti
     private lateinit var kawalPemain: KawalPemain
 
@@ -47,8 +48,15 @@ class SkrinUtama : Screen {
         Gdx.gl.glClearColor(0f, 0f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
 
+        // lock and center camera to player's pos
+        kamera.position.set(player.pos.x, player.pos.y, 0f)
+        kamera.update()
+
         kawalPemain.kemaskini(delta)
         player.kemaskini(delta)
+        if (!akanBerlagaDenganLayer(player.nextRect)) {
+            player.setCalculatedPosAsCurrent()
+        }
 
         renderer.setView(kamera)
         renderer.render()
@@ -93,5 +101,27 @@ class SkrinUtama : Screen {
         } else {
             Gdx.app.debug("SkrinUtama", "Peta tak diperolehi: $path")
         }
+    }
+
+    private fun akanBerlagaDenganLayer(rect: Rectangle): Boolean {
+        val layer = peta?.layers?.get(MAP_COLLISION_LAYER)
+        return if (layer != null) akanBerlaga(rect, layer) else false
+    }
+
+    private fun akanBerlaga(rect: Rectangle, layer: MapLayer): Boolean {
+        // convert bound dalam kaki unit ke pixel
+        rect.setPosition(rect.x / kpp, rect.y / kpp)
+        for (i in 0 until layer.objects.count) {
+            val obj = layer.objects[i]
+            if (obj is RectangleMapObject && rect.overlaps(obj.rectangle)) {
+                return true
+            }
+        }
+        return false
+    }
+
+    companion object {
+        const val kpp = 1 / 16f // 1 kaki = 16 piksel
+        private const val MAP_COLLISION_LAYER = "MAP_COLLISION_LAYER"
     }
 }
